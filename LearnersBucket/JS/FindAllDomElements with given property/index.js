0bd44ab6d795abc350ef11c4f8d451c9c55cff02
Yash Gupta <yash.gupta2@walmart.com>
2023-08-18 23:18:19 +0530

    JS

---
+++ LearnersBucket/JS/FindAllDomElements with given property/index.js
@@ -0,0 +1,14 @@
+/*
+https://www.youtube.com/watch?v=EZnVU6D3XNQ&list=PL_KW_uw2ITn8xWRkGZjKTfb-9xj_pJfgT&index=23
+
+Amazon Frontend machine coding Interview | Find all the DOM elements with given property
+
+We have to find all the DOM elements with the given color property. The color be in HEX, RGB, or text format.
+
+https://leetcode.com/discuss/interview-question/2127273/amazon-fee-phone-screen-rejected
+
+
+Input: findAllEle('color', '#fff');
+Output: Array of elements matching this color
+
+*/
