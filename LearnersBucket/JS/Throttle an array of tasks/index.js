ff4a112dbd6055a28577b0f81cc898601f171e56
Yash Gupta <yash.gupta2@walmart.com>
2023-08-22 00:03:53 +0530

    Throttle an array

---
+++ LearnersBucket/JS/Throttle an array of tasks/index.js
@@ -0,0 +1,14 @@
+/*
+
+https://leetcode.com/discuss/interview-question/346402/Uber-or-Phone-Screen-or-Design-a-Throttler
+
+https://stackoverflow.com/questions/49826332/simple-task-runner-in-javascript-with-waiting?rq=1
+
+https://www.youtube.com/watch?v=4iu9rQBCAGg&list=PL_KW_uw2ITn8xWRkGZjKTfb-9xj_pJfgT&index=32
+
+
+throttle([1,2,3,4,5,6,7,8], 2, (task) => {
+    console.log(task) --> [1,2], [3,4], [5,6], [7,8]
+}, 2000)
+
+*/
