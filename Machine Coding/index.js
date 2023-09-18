17d828365cc273c8cf66f15eefd1d80c09637f43
Yash Gupta <yash.gupta2@walmart.com>
2023-09-19 00:35:53 +0530

    System Design

---
+++ Machine Coding/index.js
@@ -0,0 +1,15 @@
+/*
+
+1. Build a Autocomplete
+2. Build a modal
+3. Create tooltip
+4. Build a chat application
+5. Build a cart 
+6. build a product listing page
+7. Build a customizable form
+8. Search
+
+add more
+
+
+*/
---
+++ System Design/Chirag Goel/4 Frontend System Design Interview /index.js
@@ -0,0 +1,21 @@
+/*
+
+https://www.youtube.com/watch?v=QemIfzcEeMM&list=PL4CFloQ4GGWICE0Tz6iXKfN3XWkXRlboU&index=4
+
+
+Things to consider in front end system desing interview
+
+1. Requirements
+2. Scoping
+3. Tech choices
+4. Component Architecture
+5. Data API, Protocols and Implementation 
+
+See the Image for more drilldown knowledge
+
+
+Difference between mono repo vs micro front end ?
+
+
+
+*/
