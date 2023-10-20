802c821b6f595897d335d17ac72a6f764375c22d
Yash Gupta <yash.gupta2@walmart.com>
2023-10-21 00:14:25 +0530

    Asset Optimization

--- System Design/Chirag Goel/10 Optimize Assets Performance.js/index.js
+++ System Design/Chirag Goel/10 Optimize Assets Performance.js/index.js
@@ -2,4 +2,7 @@
 
 https://www.youtube.com/watch?v=9JDlZxR8gVw&list=PL4CFloQ4GGWICE0Tz6iXKfN3XWkXRlboU&index=10
 
+
+https://medium.com/swlh/critical-rendering-path-in-react-the-important-yet-overlooked-performance-metric-32f6bb18869c
+
 */
